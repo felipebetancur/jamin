802f24610bc16c7eb808578e3e3fa83011d96bf7
jdepner <jdepner>
2003-05-04 15:29:01 +0000

    Just added public domain comment to spline.c.

--- src/spline.c
+++ src/spline.c
@@ -1,3 +1,9 @@
+/*
+    This source is in the public domain.  It is distributed in the hope that 
+    it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 #include <stdio.h>
 #include <math.h>
 
