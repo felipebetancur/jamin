babee8fd2277bb5c06cfd74112ee0d180f5439bb
jdepner <jdepner>
2003-07-22 23:28:29 +0000

    Forgot one.

--- src/help.c
+++ src/help.c
@@ -1,3 +1,25 @@
+/*
+ *  help.c -- Help message dialog for the JAMin (JACK Audio Mastering 
+ *            interface) program.
+ *
+ *  Copyright (C) 2003 Jan C. Depner.
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,7 +34,7 @@
 #include "help.h"
 
 
-static GtkWidget *help_dialog;
+static GtkWidget *help_dialog = NULL;
 
 
 void help_message (char *string)
