027b0c4533c4ae4c2f5f925aa00de1d1661deeb5
theno23 <theno23>
2004-04-12 22:30:52 +0000

    Removed debug code

--- src/spline.c
+++ src/spline.c
@@ -146,16 +146,10 @@ void spline_cof (float *x, float *y, int pos, float *coeffs)
 void spline (float *x, float *y, int pos, float x_pos, float *y_pos, float *ater)
 {
     int            endloop1, endloop2;
-    //float          coeffs[SPLINE_ROW + 1][SPLINE_COL];
-    float          *coeffs[SPLINE_ROW + 1];
+    float          coeffs[SPLINE_ROW + 1][SPLINE_COL];
     float 	   *contig;
     static int     valpos = 0;
     int i;
-
-    contig = calloc((SPLINE_ROW + 1) * SPLINE_COL, sizeof(float)); 
-    for (i=0; i<SPLINE_ROW + 1; i++) {
-	coeffs[i] = contig + (i * SPLINE_COL);
-    }
 
     if ((*x + *(y + pos) + *(y + (pos - 1)) + *(x + (pos - 1)) +
         *(y + (pos - 2)) - *ater) != 0.0)
